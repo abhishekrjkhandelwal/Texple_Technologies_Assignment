9429c79c273b16d3d3dfb8b910c1c91dd8d12ebe
abhishekrjkhandelwal <abhishek30199808@gmail.com>
2020-11-13 00:01:26 +0530

    Texple_Technologies_Assignment

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ let app = require('express')(),
       next();
     });
 
-    server.listen(3000,function(){
-      console.log('app listening on port: 3000');
-  });
+    var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+    
+});
+
