d83e400bf094ae62a86bd062a32df143521eee9d
abhishekrjkhandelwal <abhishek30199808@gmail.com>
2020-11-13 01:43:34 +0530

    Texple_Technologies_Assignment

--- server.js
+++ server.js
@@ -34,16 +34,15 @@ const app = express();
       next();
     });
 
- app.use(express.static(__dirname + '/dist/Texple_Technologies_Assignment'));
+ app.use(express.static(__dirname + '/dist/Employees'));
 
  app.get('*', function(req, res) {
-   res.sendFile(path.join(__dirname + '/dist/Texple_Technologies_Assignment/index.html'));
+   res.sendFile(path.join(__dirname + '/dist/Employees/index.html'));
  });
 
     var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
-
 });
 
