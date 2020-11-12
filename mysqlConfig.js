a52fa38b505413fa1ba4e6708b8ddb3a620ebffe
abhishekrjkhandelwal <abhishek30199808@gmail.com>
2020-11-13 02:19:31 +0530

    Texple_Technologies_Assignment

--- mysqlConfig.js
+++ mysqlConfig.js
@@ -2,9 +2,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'us-cdbr-iron-east-02.cleardb.net',
-    user: 'abhishek',
-    password: 'A88902616a',
-    database: 'user',
+    user: 'b7e2437887xxxa',
+    password: '0200xxx6',
+    database: 'heroku_7643ec736354xxx',
 });
 
 
